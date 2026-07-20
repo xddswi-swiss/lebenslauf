a93964282bfc1b5a624aacbf443a387008c789fa
Adnan Aydin <xanadu.ch@gmail.com>
2026-07-20 22:54:50 +0200

    Disable mobile viewport zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
